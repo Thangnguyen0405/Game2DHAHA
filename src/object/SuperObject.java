aad1f18325999297eb10f2e2eb7d044b81a2a01a
TonThatMinhVu <91868808+TonThatMinhVu@users.noreply.github.com>
2022-12-09 19:20:34 +0700

    Merge branch 'main' of C:\Object-Oriented Programming\Project156\Game2DHAHA with conflicts.

--- src/object/SuperObject.java
+++ src/object/SuperObject.java
@@ -1,14 +1,5 @@
 package object;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-import main.GamePanel;
-import main.UtilityTool;
-
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
@@ -27,22 +18,4 @@ public class SuperObject
     public String name;
     public boolean collision = false;
     public int worldX, worldY;
-    public Rectangle solidArea = new Rectangle(0,0,48,48);
-
-    public int solidAreaDefaultX = 0;
-    public int solidAreaDefaultY = 0;
-
-    UtilityTool uTool = new UtilityTool();
-    public void draw(Graphics2D t2, GamePanel gp)
-    {
-        int screenX = worldX - gp.playerT.worldX + gp.playerT.screenX;
-        int screenY = worldY - gp.playerT.worldY + gp.playerT.screenY;
-        if (       worldX + gp.tileSize> gp.playerT.worldX - gp.playerT.screenX
-                && worldX - gp.tileSize< gp.playerT.worldX+ gp.playerT.screenX
-                && worldY + gp.tileSize> gp.playerT.worldY - gp.playerT.screenY
-                && worldY - gp.tileSize< gp.playerT.worldY + gp.playerT.screenY)
-        {
-            t2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
-        }
-    }
 }
