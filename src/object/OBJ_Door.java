9fd1182b86210ad01d3247b5eb53464d9991a96c
TonThatMinhVu <91868808+TonThatMinhVu@users.noreply.github.com>
2022-12-20 01:17:50 +0700

    Update OBJ_Door.java

--- src/object/OBJ_Door.java
+++ src/object/OBJ_Door.java
@@ -1,24 +1,19 @@
 package object;
-import entity.Entity;
-import main.GamePanel;
-
 import java.io.IOException;
 import javax.imageio.ImageIO;
-public class OBJ_Door extends Entity
+public class OBJ_Door extends SuperObject
 {
-    public OBJ_Door(GamePanel gp)
+    public OBJ_Door()
     {
-        super(gp);
         name = "Door";
-        down1 = setup("/objects/door");
+        try
+        {
+            image = ImageIO.read(getClass().getResourceAsStream("/ObjectImage/door.png"));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
         collision = true;
-
-        solidArea.x = 0;
-        solidArea.y = 16;
-        solidArea.width = 48;
-        solidArea.height = 32;
-        solidAreaDefaultX = solidArea.x;
-        solidAreaDefaultY = solidArea.y;
-
     }
 }
