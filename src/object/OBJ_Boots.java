53667e286d6a4323afc59072c44bb352e1f1a86f
TonThatMinhVu <91868808+TonThatMinhVu@users.noreply.github.com>
2022-12-20 01:17:08 +0700

    Update OBJ_Boots.java

--- src/object/OBJ_Boots.java
+++ src/object/OBJ_Boots.java
@@ -1,14 +1,18 @@
 package object;
-import entity.Entity;
-import main.GamePanel;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-public class OBJ_Boots extends Entity
+public class OBJ_Boots extends SuperObject
 {
-    public OBJ_Boots(GamePanel gp)
+    public OBJ_Boots()
     {
-        super(gp);
         name = "Boots";
-        down1 = setup("/objects/boots");
+        try
+        {
+            image = ImageIO.read(getClass().getResourceAsStream("/ObjectImage/boots.png"));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
