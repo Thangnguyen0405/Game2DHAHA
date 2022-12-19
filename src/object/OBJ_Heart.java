8c50590f498a86882111d4a689ba78221670740f
TonThatMinhVu <91868808+TonThatMinhVu@users.noreply.github.com>
2022-12-20 01:18:30 +0700

    Update OBJ_Heart.java

--- src/object/OBJ_Heart.java
+++ src/object/OBJ_Heart.java
@@ -1,22 +1,28 @@
 package object;
 
-import entity.Entity;
 import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
-public class OBJ_Heart extends Entity {
-
+public class OBJ_Heart extends SuperObject {
+    GamePanel gp;
     public OBJ_Heart(GamePanel gp) {
 
-        super(gp);
+        this.gp = gp;
 
         name = "Heart";
-        image = setup("/objects/heart_full");
-        image2 = setup("/objects/heart_half");
-        image3 = setup("/objects/heart_blank");
-
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("/ObjectImage/heart_full.png"));
+            image2 = ImageIO.read(getClass().getResourceAsStream("/ObjectImage/heart_half.png"));
+            image3 = ImageIO.read(getClass().getResourceAsStream("/ObjectImage/heart_blank.png"));
+            image = uTool.scaleImage(image, gp.tileSize, gp.tileSize);
+            image2 = uTool.scaleImage(image2, gp.tileSize, gp.tileSize);
+            image3 = uTool.scaleImage(image3, gp.tileSize, gp.tileSize);
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+        solidArea.x = 5;
     }
 }
